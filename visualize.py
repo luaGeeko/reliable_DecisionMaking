ff051809a405559fea3c0ec5c00d85edf3482833
Klara Gerlei <klarizsofi@gmail.com>
2020-07-25 21:22:38 +0100

    Make more sanity check plots
    activity of dummy neurons on different behaviours

--- visualize.py
+++ visualize.py
@@ -1,10 +1,12 @@
 from matplotlib import pyplot as plt
+import math_utility
 import numpy as np
 import os
+import pandas as pd
+from scipy.ndimage.filters import uniform_filter1d
 
 
 dirname = os.path.dirname(__file__)
-
 
 # source: https://colab.research.google.com/github/NeuromatchAcademy/course-content/blob/master/projects/load_steinmetz_decisions.ipynb#scrollTo=mmOarX5w16CR
 def plot_population_average(dat, file_name='population_average'):
@@ -25,6 +27,7 @@ def plot_population_average(dat, file_name='population_average'):
     if not os.path.isdir(dirname + '/figures/'):
         os.mkdir(dirname + '/figures/')
     plt.savefig(dirname + '/figures/' + file_name + '.png')
+    plt.close()
 
 
 def plot_random_examples(data, file_name='random_examples', number_of_cells_to_plot=15):
@@ -37,6 +40,37 @@ def plot_random_examples(data, file_name='random_examples', number_of_cells_to_p
         plt.ylabel('Normalized firing rate')
         plt.title('Random example neurons')
 
-
     plt.savefig(dirname + '/figures/' + file_name + '.png')
+    plt.close()
+
+
+def plot_example_from_each_neuron_type(data, file_name='type_examples'):
+    types = np.unique(data['dummy_type'])
+    for dummy_type in types:
+        type_boolean_mask = pd.Series(data['dummy_type']) == dummy_type
+        index_example_neuron = np.where(type_boolean_mask == True)[0][0]
+        right_choice = data['response'] == -1
+        example_right_trial = np.where(right_choice == True)[0][0]
+        left_choice = data['response'] == 1
+        example_left_trial = np.where(left_choice == True)[0][0]
+        data_from_right_trial = data['spks'][index_example_neuron, example_right_trial, :]
+        data_from_right_trial_filtered = math_utility.moving_average(data_from_right_trial, window=20)
+        data_from_left_trial = data['spks'][index_example_neuron, example_left_trial, :]
+        data_from_left_trial_filtered = math_utility.moving_average(data_from_left_trial, window=20)
+
+        plt.figure()
+        plt.xlabel('Time (10 ms bins)', fontsize=16)
+        plt.ylabel('Firing rate', fontsize=16)
+        plt.title('Activity of "' + dummy_type + '" neuron', fontsize=16)
+        plt.plot(data_from_left_trial, color='black', linewidth=1, alpha=0.7, label='Left choice')
+        plt.plot(data_from_right_trial, color='red', linewidth=1, alpha=0.7, label='Right choice')
+        plt.plot(data_from_right_trial_filtered, color='red', linewidth=5)
+        plt.plot(data_from_left_trial_filtered, color='black', linewidth=5)
+        plt.legend(loc="upper right", frameon=False)
+
+        plt.savefig(dirname + '/figures/' + file_name + '_' + dummy_type + '.png')
+        plt.close()
+
+
+
 
