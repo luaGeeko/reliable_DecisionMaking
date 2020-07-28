d79ddfed46031b4ffc32d02f7ebaea392ad6993b
luaGeeko <hop2work@gmail.com>
2020-07-28 17:29:24 +0200

    fixed true divide error

--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pylab as plt
 import timeit
+import scipy.stats as stats
 
 # random seed for reproducibility of experiements with dataset splits
 np.random.seed(75)
-
+# FIXED: RuntimeWarning: invalid value encountered in true_divide px = px / px.sum()
 def my_gaussian(x_points, mu, sigma):
     """
-    from: https://github.com/NeuromatchAcademy/course-content/blob/master/tutorials/W2D1_BayesianStatistics/solutions/W2D1_Tutorial1_Solution_aeeeaedf.py
     Returns normalized Gaussian estimated at points `x_points`, with parameters:
      mean `mu` and std `sigma`
     Args:
@@ -18,10 +18,10 @@ def my_gaussian(x_points, mu, sigma):
     Returns:
         (numpy array of floats) : normalized Gaussian evaluated at `x`
     """
-    px = np.exp(- 1/ 2 / sigma ** 2 * (mu - x_points) ** 2)
-    px = px / px.sum()
+    #px = np.exp(- 1/ 2 / sigma ** 2 * (mu - x_points) ** 2)
+    #px = px / px.sum()
+    px = stats.norm.pdf(x_points, mu, sigma)
     return px
-
 
 def get_mixture_of_random_gaussians(number_of_gaussians, x_points):
     mixture = np.zeros(len(x_points))
@@ -29,10 +29,9 @@ def get_mixture_of_random_gaussians(number_of_gaussians, x_points):
         random_mu = np.random.uniform(0, len(x_points))
         random_sigma = np.random.uniform(0, len(x_points)/4)
         new_gauss = my_gaussian(x_points, random_mu, random_sigma)
-        new_gauss = new_gauss / np.linalg.norm(new_gauss)
+        #new_gauss = new_gauss / np.linalg.norm(new_gauss)
         mixture += new_gauss
     return mixture
-
 
 def moving_average(x, window):
     return np.convolve(x, np.ones(window), 'valid') / window
@@ -43,7 +42,7 @@ def time_cal(func, *args, **kwargs):
     return inner_func
 
 def main():
-    mix = get_mixture_of_random_gaussians(15, np.arange(250))
+    mix = my_gaussian(np.arange(250), 2, 1)
     plt.plot(mix)
     plt.xlabel("TIme bins")
     plt.ylabel("pdf")
