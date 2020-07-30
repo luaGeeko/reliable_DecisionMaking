d1eb88077dd96babb6ebcd7d6a2d384d23e8b7b9
Klara Gerlei <klarizsofi@gmail.com>
2020-07-30 18:18:35 +0100

    Get version from master

--- split_test_and_training_data.py
+++ split_test_and_training_data.py
@@ -1,143 +1,88 @@
+""" This module splits the dataset into training, validation and test sets. Test set is never to be seen by the model. While training, validation set is used to validate 
+    how well model performs. """
+
 import copy
-import load_data
+from load_data import load_mouse_data
 import numpy as np
+import coloredlogs, logging
 
+# debugging
+logger = logging.getLogger("split_dataset")
+coloredlogs.install(level='INFO', logger=logger)
 
-def make_binary_array_with_trial_types(number_of_trials, test_percentage):
-    """
-        Makes binary array where the length of the array is the number of trials and the proportion of 1s is
-        test percentage.
+# random seed for reproducibility of experiements with dataset splits
+np.random.seed(75)
 
-        Args:
-            number_of_trials (int) : number of trials
-            test_percentage (float) : the percentage of trials per neuron that will be test trials
-                                       0 >= test_percentage <= 1
-        Returns:
-            binary list where 1 represents a test trial and 0 a training trial
-
-        """
-    trial_type = np.zeros(number_of_trials)
-    number_of_test_rows = int(round(number_of_trials * test_percentage / 100, 0))
-    random_indices = np.random.choice(number_of_trials, number_of_test_rows, replace=False)
-    trial_type[random_indices] = 1
-    return trial_type
-
-
-def label_test_and_training_data(all_data, test_percentage=20):
-    """
-    Adds a column 'test_data' to each trial with a binary array where 1 = the trial is test data, 0 = the trial
-    is training data. For each neuron, it will classify 'test_percentage' proportion of trials as test trials.
-
+def create_dataset_split_indices(data, train_size):
+    """ 
+    splits the data with given percentage in two parts [ train / test]. the given percentage is kept for training set 
+    and remaining for test set
     Args:
-        all_data (class) : all simulated or mouse data
-        test_percentage (float) : the percentage of trials per neuron that will be test trials
-                                   0 >= test_percentage <= 1
+        train_size (float) : train percentage to be kept for training data
     Returns:
-        class, the input data with an additional column
+        dict holding the session ids as keys and their corresponding trial indices created for train and test set 
     """
-    print('Labelling data for splitting.')
-    print('WARNING: the passive trials will not be split by this.')
-    np.random.seed(800)  # make sure that it always chooses the same trials as test trials
+    dataset_split_indices_holder = {}
+    # NOTE: in matlab the index starts from 1 in python its starts from 0
+    for session_id in range(data.shape[0]):
+        num_spike_trials = data[session_id]['spks']
+        # get trails for the neurons
+        session_id_trial_indices = np.random.permutation(num_spike_trials.shape[1])
+        # NOTE: split done for only train and test sets, I think validation set is also useful can easily be modified later
+        #training_idx, temp_valid_idx = np.split(session_id_indices, [int(train_per * session_id_indices.shape[0])])
+        training_trials_idx, test_trails_idx = np.split(session_id_trial_indices, [int(train_size * session_id_trial_indices.shape[0])])
+        assert session_id_trial_indices.shape[0] == (training_trials_idx.shape[0] + test_trails_idx.shape[0])
+        dataset_split_indices_holder[session_id] = {'train': training_trials_idx, 'test': test_trails_idx}
 
-    for session_id in range(len(all_data)):
-        trials_in_session = all_data[session_id]['spks']  # [# of neurons x trials x time (2.5 sec)]
-        number_of_trials = trials_in_session.shape[1]  # number of trials in session
-        # generate random series of 0s and 1s
-        trial_types = make_binary_array_with_trial_types(number_of_trials, test_percentage)
-        all_data[session_id]['test_data'] = trial_types
-
-    return all_data
+    return dataset_split_indices_holder
 
 
-def get_data_for_trial_indices(all_data, session_id, trial_indices):
-    """
-    Overwrites data with data from selected trials only
-
+def get_data_for_trial_indices(data, session_id, trial_indices):
+    """ 
+    extracts the indices from data according to given set type [train/test]
     Args:
-        all_data (class) : all simulated or mouse data
-        session_id (int) : id of session to update
-        trial_indices (list) : list of trial indices to keep in data
-
+        data (numpy array) : simulated or mouse data
+        session_id (int) : session id for which data is to reformed
+        split_indices (dict) : a dictionary holding indices for given set type [train/test] for all the sessions
     Returns:
-        numpy array, session with updated data
+        numpy array holding data for either training / test set for all sessions
     """
-    all_data[session_id]['spks'] = all_data[session_id]['spks'][:, trial_indices, :]
-    all_data[session_id]['wheel'] = all_data[session_id]['wheel'][:, trial_indices, :]
-    all_data[session_id]['pupil'] = all_data[session_id]['pupil'][:, trial_indices, :]
-    all_data[session_id]['lfp'] = all_data[session_id]['lfp'][:, trial_indices, :]
-    all_data[session_id]['contrast_right'] = all_data[session_id]['contrast_right'][trial_indices]
-    all_data[session_id]['contrast_left'] = all_data[session_id]['contrast_left'][trial_indices]
-    all_data[session_id]['gocue'] = all_data[session_id]['gocue'][trial_indices]
-    all_data[session_id]['response_time'] = all_data[session_id]['response_time'][trial_indices]
-    all_data[session_id]['feedback_time'] = all_data[session_id]['feedback_time'][trial_indices]
-    all_data[session_id]['feedback_type'] = all_data[session_id]['feedback_type'][trial_indices]
-    all_data[session_id]['response'] = all_data[session_id]['response'][trial_indices]
-    all_data[session_id]['test_data'] = all_data[session_id]['test_data'][trial_indices]
-    return all_data[session_id]
+    temp_holder = {}
+    temp_holder['spks'] = data[session_id]['spks'][:, trial_indices, :]
+    temp_holder['wheel'] = data[session_id]['wheel'][:, trial_indices, :]
+    temp_holder['pupil'] = data[session_id]['pupil'][:, trial_indices, :]
+    temp_holder['lfp'] = data[session_id]['lfp'][:, trial_indices, :]
+    temp_holder['contrast_right'] = data[session_id]['contrast_right'][trial_indices]
+    temp_holder['contrast_left'] = data[session_id]['contrast_left'][trial_indices]
+    temp_holder['gocue'] = data[session_id]['gocue'][trial_indices]
+    temp_holder['response_time'] = data[session_id]['response_time'][trial_indices]
+    temp_holder['feedback_time'] = data[session_id]['feedback_time'][trial_indices]
+    temp_holder['feedback_type'] = data[session_id]['feedback_type'][trial_indices]
+    temp_holder['response'] = data[session_id]['response'][trial_indices]
+    return temp_holder
+
+def get_training_data(data, split_indices):
+    """ creates training set """
+    train_data = {}
+    for session_id in range(data.shape[0]):
+        train_data[session_id] = get_data_for_trial_indices(data, session_id, split_indices[session_id]['train'])
+    logger.info("training data created")
+    return train_data
+
+def get_test_data(data, split_indices):
+    """ creates test set """
+    test_data = {}
+    for session_id in range(data.shape[0]):
+        test_data[session_id] = get_data_for_trial_indices(data, session_id, split_indices[session_id]['test'])
+    logger.info("test data created")
+    return test_data
 
 
-def get_test_data(all_data_in):
-    """
-    Get test data set. The seed is set so this should always give the same result
-
-    Args:
-        all_data (class) : all simulated or mouse data
-
-    Returns:
-        test data in the same format as the input data
-    """
-
-    all_data = copy.deepcopy(all_data_in)
-    all_data = label_test_and_training_data(all_data)
-    # filter data set and return trials that are in the test data
-    for session_id in range(len(all_data)):
-        trial_types = all_data[session_id]['test_data']
-        indices_of_test_trials = np.where(trial_types == 1)[0]
-        all_data[session_id] = get_data_for_trial_indices(all_data, session_id, indices_of_test_trials)
-    return all_data
-
-
-def get_training_data(all_data_in):
-    """
-    Get training data set. The seed is set so this should always give the same result
-
-    Args:
-        all_data (class) : all simulated or mouse data
-
-    Returns:
-        training data in the same format as the input data
-    """
-
-    all_data = copy.deepcopy(all_data_in)
-    all_data = label_test_and_training_data(all_data)
-    # filter data set and return trials that are in the training data
-    for session_id in range(len(all_data)):
-        trial_types = all_data[session_id]['test_data']
-        indices_of_training_trials = list(np.where(trial_types == 0)[0])
-        all_data[session_id] = get_data_for_trial_indices(all_data, session_id, indices_of_training_trials)
-    return all_data
-
-
-def get_test_and_training_data(all_data):
-    """
-    Return test and training data sets.
-    Args:
-        all_data (class, numpy array of dictionaries) : all simulated or mouse data
-
-    Returns:
-        training and test data in the same format as the input data
-    """
-    test_data = get_test_data(all_data)
-    training_data = get_training_data(all_data)
-    return test_data, training_data
-
-
-# this is just here for testing
-def main():
-    all_data = load_data.load_mouse_data()
-    test_data = get_test_data(all_data)
-    training_data = get_training_data(all_data)
-
-
-if __name__ == '__main__':
-    main()
+def train_test_split(train_size: float = 0.8):
+    """ split the dataset into training and test set """
+    # load dataset
+    data = load_mouse_data()
+    split_trial_indices = create_dataset_split_indices(data, train_size)
+    train_data = get_training_data(data, split_trial_indices)
+    test_data = get_test_data(data, split_trial_indices)
+    return train_data, test_data
